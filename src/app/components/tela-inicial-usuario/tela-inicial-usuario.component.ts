030b5f532be77a376d57fd96ef9988b8db6dbf52
Filipe Rodrigues <rodrigues.filipe@academico.ifpb.edu.br>
2024-08-21 09:40:51 -0300

    Atualizando a lógica da página do usuário 🛠

--- src/app/components/tela-inicial-usuario/tela-inicial-usuario.component.ts
+++ src/app/components/tela-inicial-usuario/tela-inicial-usuario.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { Usuario } from './../../shared/models/usuario';
+import { LocalStorageService } from './../../shared/services/local-storage.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router} from '@angular/router';
 import { ThemeModeService } from '../../shared/services/theme-mode.service';
+import { UsuarioService } from '../../shared/services/usuario.service';
 
 @Component({
   selector: 'app-tela-usuario',
   templateUrl: './tela-inicial-usuario.component.html',
   styleUrl: './tela-inicial-usuario.component.scss'
 })
-export class TelaInicialUsuarioComponent {
-  constructor(private rotaAtual: ActivatedRoute, private roteador: Router, public themeService: ThemeModeService) { }
+  
+export class TelaInicialUsuarioComponent implements OnInit{
+  constructor(private rotaAtual: ActivatedRoute, private roteador: Router, public themeService: ThemeModeService, private localStorageService: LocalStorageService, private usuarioService: UsuarioService) {
+    const idUsuario = this.rotaAtual.snapshot.paramMap.get('id') || undefined;
+      if (idUsuario) {
+        this.usuarioService.usuario = this.usuarioService.getUsuario(idUsuario);
+        localStorageService.atualizarUsuario(this.usuarioService.usuario);
+      }
+  }
 
   toggleThemeMode() {
     this.themeService.updateThemeMode();
