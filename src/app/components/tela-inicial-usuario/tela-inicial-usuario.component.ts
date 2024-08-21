f56b8cdc901d8a5088c9062b84b4e4e1c4d2bdaa
Filipe Rodrigues <rodrigues.filipe@academico.ifpb.edu.br>
2024-08-21 09:56:23 -0300

    Ajustando o tratamento de dados do id de usuário 🛠

--- src/app/components/tela-inicial-usuario/tela-inicial-usuario.component.ts
+++ src/app/components/tela-inicial-usuario/tela-inicial-usuario.component.ts
@@ -10,10 +10,10 @@ import { UsuarioService } from '../../shared/services/usuario.service';
   templateUrl: './tela-inicial-usuario.component.html',
   styleUrl: './tela-inicial-usuario.component.scss'
 })
-  
+
 export class TelaInicialUsuarioComponent implements OnInit{
   constructor(private rotaAtual: ActivatedRoute, private roteador: Router, public themeService: ThemeModeService, private localStorageService: LocalStorageService, private usuarioService: UsuarioService) {
-    const idUsuario = this.rotaAtual.snapshot.paramMap.get('id') || undefined;
+    const idUsuario = Number (this.rotaAtual.snapshot.paramMap.get('id'))  || undefined;
       if (idUsuario) {
         this.usuarioService.usuario = this.usuarioService.getUsuario(idUsuario);
         localStorageService.atualizarUsuario(this.usuarioService.usuario);
